beef0fb7183a27fd6dd2637fee050938bf2011fb
santhosh101066 <ewaste101066@gmail.com>
2022-11-20 20:42:53 +0530

    resoving

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 self.addEventListener("fetch", (event) => {
     if(event.request.url==event.request.referrer+'offline.jpg'){
-        event.respondWith(fetch(event.request).catch(err=>self.caches.open(cachename).then(cache=>cache.match('/offline.jpg'))))
+        event.respondWith(fetch(event.request).catch(err=>self.caches.open(cachename).then(cache=>cache.match('offline.jpg'))))
     }
-    event.respondWith(fetch(event.request).catch(err => self.caches.open(cachename).then(cache => cache.match('/offline.html'))))
+    event.respondWith(fetch(event.request).catch(err => self.caches.open(cachename).then(cache => cache.match('offline.html'))))
 })
 
 
@@ -11,8 +11,8 @@ self.addEventListener('install', (event) => {
     console.log("initalizing")
     event.waitUntil(caches.open(cachename)
         .then((cache) => {
-            cache.add('/offline.html')
-            cache.add('/offline.jpg')
+            cache.add('offline.html')
+            cache.add('offline.jpg')
         })
         .catch(err => console.error(err)))
 })
