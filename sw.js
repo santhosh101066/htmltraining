893d53131b22d50b43369a097268157230dd80bd
santhosh101066 <ewaste101066@gmail.com>
2022-11-21 14:47:11 +0530

    today

--- sw.js
+++ sw.js
@@ -1,12 +1,29 @@
-self.addEventListener("fetch", (event) => {
-    if(event.request.url==event.request.referrer+'offline.jpg'){
-        event.respondWith(fetch(event.request).catch(err=>self.caches.open(cachename).then(cache=>cache.match('offline.jpg'))))
-    }
-    event.respondWith(fetch(event.request).catch(err => self.caches.open(cachename).then(cache => cache.match('offline.html'))))
+// self.addEventListener("fetch", (event) => {
+//     if(event.request.url==event.request.referrer+'offline.jpg'){
+//         event.respondWith(fetch(event.request).catch(err=>self.caches.open(cachename).then(cache=>cache.match('offline.jpg'))))
+//     }
+//     else{
+//         event.respondWith(fetch(event.request).catch(err => self.caches.open(cachename).then(cache => cache.match('offline.html'))))
+//     }
+// })
+
+
+self.addEventListener('fetch', (event) => {
+    let path= new URL(event.request.url).pathname
+    console.log(path);
+    const run=async ()=>{
+        const cache= await fetch(event.request).catch(err=> caches.match(path))
+        if (cache) return cache
+        return  fetch(event.request).catch(err=> caches.match('offline.html'))
+     } 
+    event.respondWith(run())
+  
+
+
 })
 
-
 let cachename = 'htmltraining'
+
 self.addEventListener('install', (event) => {
     console.log("initalizing")
     event.waitUntil(caches.open(cachename)
